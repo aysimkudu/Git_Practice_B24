1c5a04ea767dedc410ef590e649a9563c71e4b81
aysim <kuduaysim@gmail.com>
2021-10-03 15:51:47 -0500

    added hello wolrd class

--- src/tests/HeekoWorldTest.java
+++ src/tests/HeekoWorldTest.java
@@ -4,5 +4,8 @@ public class HeekoWorldTest {
     public static void main(String[] args) {
 
         System.out.println("hello world");
+
+
+        System.out.println("one more hello world");
     }
 }
